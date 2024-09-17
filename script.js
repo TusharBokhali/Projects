8e2ed27288eb3c39363fb78754ab2e581a5b2b1b
TusharBokhali <variyatushar2023.katargam@gmail.com>
2024-09-17 12:22:53 +0530

    First

---
+++ script.js
@@ -0,0 +1,9 @@
+// let userSelecte = 
+function foo() {
+ return  window.getSelection("hell");
+    // alert(selObj);
+    // const selRange = selObj.getRangeAt(0);
+    // do stuff with the range
+  }
+  console.log(foo());
+  
